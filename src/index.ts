3cc048e3ab2c257d8c60621dcc14815a17b108be
1amageek <nori@stamp.team>
2022-12-19 19:42:00 +0900

    Fix

--- src/index.ts
+++ src/index.ts
@@ -127,8 +127,8 @@ export const propagate = (
 
 const compress = (name: string, list: string[]) => {
   if (list.includes(name)) {
-    if (name.length > 1) {
-      return name.slice(0, 2)
+    if (name.length > 6) {
+      return name.slice(0, 6)
     }
     return name[0]
   } else {
